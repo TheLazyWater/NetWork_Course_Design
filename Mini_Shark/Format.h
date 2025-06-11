753dacf85c86ffa67da35c3ea97803f0a939066c
Truman_4 <3296872794@qq.com>
2025-06-11 12:32:27 +0800

    暂存节点

---
+++ Mini_Shark/Format.h
@@ -0,0 +1,54 @@
+#ifndef FORMAT_H
+#define FORMAT_H
+
+typedef unsigned char u_char;     // 1 byte
+typedef unsigned short u_short;   // 2 byte
+typedef unsigned int u_int;       // 4 byte
+typedef unsigned long u_long;     // 4 byte
+
+// Ethernet protocol format
+/*
++-------------------+-----------------+------+
+|       6 byte      |     6 byte      |2 byte|
++-------------------+-----------------+------+
+|destination address|  source address | type |
++-------------------+-----------------+------+
+*/
+
+typedef struct ether_header{   // 14 byte
+    u_char ether_des_host[6];  // destination addr [6 byte]
+    u_char ether_src_host[6];  // source addr [6 byte]
+    u_short ether_type;        // type [2 byte]
+}ETHER_HEADER;
+
+// Ipv4 header
+/*
++-------+-----------+---------------+-------------------------+
+| 4 bit |   4 bit   |    8 bit      |          16 bit         |
++-------+-----------+---------------+-------------------------+
+|version|head length|  TOS/DS_byte  |        total length     |
++-------------------+--+---+---+----+-+-+-+-------------------+
+|          identification           |R|D|M|    offset         |
++-------------------+---------------+-+-+-+-------------------+
+|       ttl         |     protocal  |         checksum        |
++-------------------+---------------+-------------------------+
+|                   source ip address                         |
++-------------------------------------------------------------+
+|                 destination ip address                      |
++-------------------------------------------------------------+
+*/
+typedef struct ip_header{           // 20 byte
+    u_char versiosn_head_length;    // version [4 bit] and length of header [4 bit]
+    u_char TOS;                     // TOS/DS_byte [1 byte]
+    u_short total_length;           // ip package total length [2 byte]
+    u_short identification;         // identification [2 byte]
+    u_short flag_offset;            // flag [3 bit] and offset [13 bit]
+    u_char ttl;                     // TTL [1 byte]
+    u_char protocol;                // protocal [1 byte]
+    u_short checksum;               // checksum [2 byte]
+    u_int src_addr;                 // source address [4 byte]
+    u_int des_addr;                 // destination address [4 byte]
+}IP_HEADER;
+// Tcp header
+
+#endif // FORMAT_H
---
+++ Mini_Shark/datapackage.h
@@ -0,0 +1,43 @@
+#ifndef DATAPACKAGE_H
+#define DATAPACKAGE_H
+#include <QString>
+#include "Format.h"
+
+class DataPackage
+{
+private:
+    u_int data_length; // data pakage length
+    QString timeStamp; // timestamp of package
+    QString info;      // a breif introduction of package
+    int packageType;   // type
+
+public:
+    const u_char *pkt_content; // root pointer of package data
+
+protected:
+    /*
+     * turn the byteArray to QString
+    */
+    static QString byteToHex(u_char*str,int size);
+public:
+    // Construction and destruction
+    DataPackage();
+    ~DataPackage() = default;
+
+    // set the var
+    void setDataLength(unsigned int length);                    // set the package length
+    void setTimeStamp(QString timeStamp);                       // set timestamp
+    void setPackageType(int type);                              // set package type
+    void setPackagePointer(const u_char *pkt_content,int size); // set package pointer
+    void setPackageInfo(QString info);                          // set package information
+
+    // get the var
+    QString getDataLength();                  // get package length
+    QString getTimeStamp();                   // get timestamp
+    QString getPackageType();                 // get package type
+    QString getInfo();                        // get a breif package information
+    QString getSource();                      // get the source address of package
+    QString getDestination();                 // get the destination address of package
+};
+
+#endif // DATAPACKAGE_H
